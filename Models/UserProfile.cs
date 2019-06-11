293887b57c56f785fb12f306e1b0b6f1191640ba
karthik pamidimukkala <karthiksai231@gmail.com>
2019-06-10 22:31:25 -0400

    Adding Dialogs

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -5,6 +5,10 @@ namespace EchoBot.Models
     public class UserProfile
     {
         public string UserName { get; set; }
+        public string KnownAs { get; set; }
         public string DateOfBirth { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Password { get; set; }
     }
 }
